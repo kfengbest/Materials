a3aae38491cca8a30533d58446cfe0913f4df971
= <kaven.feng@autodesk.com>
2014-03-13 18:05:42 +0800

    Load classifications for landing page.

---
+++ Materials/LandingPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  LandingPageViewController.h
+//  Materials
+//
+//  Created by Kaven Feng on 3/13/14.
+//  Copyright (c) 2014 Kaven Feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LandingPageViewController : UITableViewController
+
+@end
